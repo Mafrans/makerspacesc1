6c5c41eb6af2e92da493b1f2df6a05364097cf28
Mafrans <malte.kluft@gmail.com>
2019-02-14 23:33:47 +0100

    Add multiple pages and swap between them

--- viewport.js
+++ viewport.js
@@ -1,13 +1,37 @@
+let page = 0;
+let page_amount = 2;
+
 $(document).ready(function () {
+	switchPage();
     setTimeout(function() {
         location.reload();
     }, 3600000);
+	
+    setTimeout(function run() {
+		switchPage();
+		setTimeout(run, 10000);
+    }, 10000);
     update();
 });
 
 $(window).resize(function () {
     update();
 });
+
+
+function switchPage() {	
+	page++;
+	console.log(page);
+	if(page >= page_amount) page = 0;
+	
+	for(var i = 0; i < page_amount; i++) {
+		if(i == page) continue;
+		$(".page-" + i).hide();
+	}
+	
+	$(".page-" + page).show();	
+}
+
 
 function update() {
     let wHeight = $(window).height();
@@ -28,4 +52,5 @@ function update() {
 
     let objy = $(".centered-y").filter(":visible");
     objy.css("top", wHeight/2 - objy.height()/2);
+	
 }
